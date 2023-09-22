897fa10bf4c04be04452882c03c21a4e7c91af19
nvhiii <nahikhan16@gmail.com>
2023-09-22 17:11:50 -0400

    added some beginning ps scripting

---
+++ Operating systems/powershell_practice/test1.ps1
@@ -0,0 +1,12 @@
+# trying out some cmdlets, verb-noun combos, and init
+
+Write-Host "Hello, Nahi" # this prints to terminal
+
+$computerinfo = Get-ComputerInfo
+
+"nahi" -ceq "Nahi" # auto returns value
+
+Write-Host "The name of your computer is $computerinfo"
+
+# Some operators: eq, ne, gt, lt, ge, le, Like, NotLike, Match, NotMatch, Contains, NotContains, In, NotIn, Replace
+
